f7319031501650660dd3fb99eb213f872f39103e
INYONG KIM <ohnyong87@gmail.com>
2023-09-12 22:35:26 +0900

    test : Add BeforeAfterTest

---
+++ src/test/java/com/sparta/junit5practice/BeforeAfterTest.java
@@ -0,0 +1,34 @@
+package com.sparta.junit5practice;
+
+import org.junit.jupiter.api.*;
+
+public class BeforeAfterTest {
+    @BeforeEach
+    void setUp() {
+        System.out.println("각각의 테스트 코드가 실행되기 전에 수행");
+    }
+
+    @AfterEach
+    void tearDown() {
+        System.out.println("각각의 테스트 코드가 실행된 후에 수행\n");
+    }
+
+    @BeforeAll
+    static void beforeAll() {
+        System.out.println("모든 테스트 코드가 실행되기 전에 최초로 수행\n");
+    }
+
+    @AfterAll
+    static void afterAll() {
+        System.out.println("모든 테스트 코드가 수행된 후 마지막으로 수행");
+    }
+
+    @Test
+    void test1(){
+
+    }
+    @Test
+    void test2(){
+
+    }
+}
